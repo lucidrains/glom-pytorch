48143c36bbc54d0cc7179317fe4ac1f610e9fe70
Phil Wang <lucidrains@gmail.com>
2021-03-04 19:55:39 -0800

    image to tokens, readying the bottom layer

--- glom_pytorch/glom_pytorch.py
+++ glom_pytorch/glom_pytorch.py
@@ -3,6 +3,7 @@ import torch.nn.functional as F
 from torch import nn, einsum
 
 from einops import rearrange, repeat
+from einops.layers.torch import Rearrange
 
 # helpers
 
@@ -13,13 +14,23 @@ def exists(val):
 
 class Glom(nn.Module):
     def __init__(
-    	self,
-    	*,
-    	dim = 512,
-    	layers = 6
+        self,
+        *,
+        dim = 512,
+        layers = 6,
+        image_size = 224,
+        patch_size = 14
     ):
         super().__init__()
-        self.embeddings = nn.Parameter(torch.randn(layers, dim))
+        self.image_to_tokens = nn.Sequential(
+            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_size, p2 = patch_size),
+            nn.Linear(patch_size ** 2 * 3, dim)
+        )
 
-    def forward(self, x):
-        return x
+        self.init_embeddings = nn.Parameter(torch.randn(layers, dim))
+
+    def forward(self, img):
+        b = img.shape[0]
+        tokens = self.image_to_tokens(img)
+        print(tokens.shape)
+        return tokens
