1c1150caaaedba2741a925251a27a9efb7842644
Phil Wang <lucidrains@gmail.com>
2021-03-04 20:06:29 -0800

    setup bottom layer state (from image tokens), which, at each iteration, should feed up the levels, which are also initialized

--- glom_pytorch/glom_pytorch.py
+++ glom_pytorch/glom_pytorch.py
@@ -22,15 +22,25 @@ class Glom(nn.Module):
         patch_size = 14
     ):
         super().__init__()
+        # bottom level - incoming image, tokenize and add position
+        num_patches = (image_size // patch_size) ** 2
         self.image_to_tokens = nn.Sequential(
             Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_size, p2 = patch_size),
             nn.Linear(patch_size ** 2 * 3, dim)
         )
+        self.pos_emb = nn.Embedding(num_patches, dim)
 
-        self.init_embeddings = nn.Parameter(torch.randn(layers, dim))
+        # initial embeddings for all levels of a column
+        self.init_levels = nn.Parameter(torch.randn(layers, dim))
 
     def forward(self, img):
-        b = img.shape[0]
+        b, device = img.shape[0], img.device
+
         tokens = self.image_to_tokens(img)
-        print(tokens.shape)
+        n = tokens.shape[1]
+
+        pos_embs = self.pos_emb(torch.arange(n, device = device))
+        bottom_level = tokens + rearrange(pos_embs, 'n d -> () n d')
+        levels = repeat(self.init_levels, 'l d -> b n l d', b = b, n = n)
+
         return tokens
